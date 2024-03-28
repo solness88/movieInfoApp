f5dcfe8960639e4a3169c3c53b77b9e60f715277
Hirokazu Kobayashi <solness.stf@gmail.com>
2024-03-28 14:02:39 +0900

    use for loop instad of forEach

--- upperRadioButton.js
+++ upperRadioButton.js
@@ -1,15 +1,14 @@
-
 var internetAccess = '';
 
 function upperRadioButton() {
   var upperRadioButtonsInternet = document.querySelectorAll('input[type="radio"][name="upperRadioButtonsInternet"]');
 
   //radio button asking whether internet distribution availability for whole edited movie
-  upperRadioButtonsInternet.forEach(function (upperRadioButtonInternet) {
-    upperRadioButtonInternet.addEventListener('change', function () {
-      internetAccess = this.value;
-    })
-  });
+  for (var i = 0; i < upperRadioButtonsInternet.length; i++) {
+    upperRadioButtonsInternet[i].addEventListener('change', function () {
+      internetAccess = this.value; // internetAccessの値を更新
+    });
+  }
 
   return internetAccess;
 }
